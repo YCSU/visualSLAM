9654191ad7056790f5f40b56e38900f2e71c8bc3
YCSU <ycsuphy@gmail.com>
2017-10-18 23:32:26 +0800

    add sophus

---
+++ ch4_useSophus/useSophus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "sophus/so3.h"
+#include "sophus/se3.h"
+
+int main( int argc, char** argv){
+
+	return 0;
+}
