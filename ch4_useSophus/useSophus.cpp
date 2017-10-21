b45b73e4214cbac5c54e3fa22351a6291316f553
YCSU <ycsuphy@gmail.com>
2017-10-21 23:55:50 +0800

    add lie algebra example

--- ch4_useSophus/useSophus.cpp
+++ ch4_useSophus/useSophus.cpp
@@ -9,6 +9,48 @@ using namespace std;
 #include "sophus/se3.h"
 
 int main( int argc, char** argv){
+  Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
+  cout << "**************** SO3 ************************" << endl;
+  // 3 ways to initialized SO(3)
+  Sophus::SO3 SO3_R(R);
+  Sophus::SO3 SO3_v(0, 0, M_PI/2);
+  Eigen::Quaterniond q(R);
+  Sophus::SO3 SO3_q(q);
 
-	return 0;
+  cout << "from matrix" << SO3_R << endl;
+  cout << "from vector" << SO3_v << endl;
+  cout << "from quaternion" << SO3_q << endl;
+  
+  // lie algebra vector
+  Eigen::Vector3d so3 = SO3_R.log();
+  cout << "so3 = " << so3.transpose() << endl;
+  cout << "so3 hat = " << endl << Sophus::SO3::hat(so3) << endl;
+  // reverse hat
+  cout << "so3 hat vee = " << Sophus::SO3::vee(Sophus::SO3::hat(so3)).transpose() << endl;
+  
+  Eigen::Vector3d update_so3(1e-4, 0, 0);
+  Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3) * SO3_R;
+  cout << "SO3 updated = " << SO3_updated << endl;
+
+  cout << "**************** SE3 ************************" << endl;
+  Eigen::Vector3d t(1, 0, 0);
+  Sophus::SE3 SE3_Rt(R, t);
+  Sophus::SE3 SE3_qt(q, t);
+  cout << "SE3 from R,t=" << SE3_Rt << endl;
+  cout << "SE3 from q,t=" << SE3_qt << endl;
+
+  typedef Eigen::Matrix<double, 6, 1> Vector6d;
+  Vector6d se3 = SE3_Rt.log();
+  cout << "se3 = " << se3.transpose() << endl;
+
+  cout << "se3 hat = " << endl <<  Sophus::SE3::hat(se3) << endl;
+  cout << "se3 hat vee = " << Sophus::SE3::vee(Sophus::SE3::hat(se3)).transpose() << endl;
+
+  Vector6d update_se3;
+  update_se3.setZero();
+  update_se3(0,0) = 1e-4d;
+  Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt;
+  cout << "SE3 updated = " << endl << SE3_updated.matrix() << endl;
+
+  return 0;
 }
